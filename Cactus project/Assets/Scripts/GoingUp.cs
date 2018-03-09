85c543522de43160aa7154914de0e5fd24f91f82
Unknown <Robin.seignolles@hotmail.fr>
2018-03-09 08:32:46 +0100

    Projet 09/03 8:32

--- Cactus project/Assets/Scripts/GoingUp.cs
+++ Cactus project/Assets/Scripts/GoingUp.cs
@@ -8,8 +8,10 @@ public class GoingUp : MonoBehaviour {
 	public Collider2D highPlatform;
 	public GameObject player;
 	public GameObject flower;
+	public GameObject goingDown;
 	public Collider2D levelCollider;
 	public float waitTime = 1f;
+	public bool isHigh;
 
 	// Use this for initialization
 	void Start () 
@@ -25,20 +27,21 @@ public class GoingUp : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D coll)
 	{
-		if(coll.gameObject.tag == "Player")
+		if(coll.gameObject.tag == "Player" && !isHigh)
 		{
 			StartCoroutine (ActiveCollider ());
-			player.GetComponent<SpriteRenderer> ().sortingOrder += 8;
-			flower.GetComponent<SpriteRenderer> ().sortingOrder += 9;
 
 		}
 	}
 
 	IEnumerator ActiveCollider()
 	{
+		player.GetComponent<SpriteRenderer> ().sortingOrder += 8;
+		flower.GetComponent<SpriteRenderer> ().sortingOrder += 9;
 		yield return new WaitForSeconds (waitTime);
+		isHigh = true;
 		highPlatform.enabled = true;
 		levelCollider.enabled = false;
-
+		this.GetComponent<Collider2D> ().enabled = false;
 	}
 }
