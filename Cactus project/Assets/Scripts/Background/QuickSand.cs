16ca4d3912bb9c14582dc65d6e1232228a8cba22
Unknown <Robin.seignolles@hotmail.fr>
2018-03-13 09:20:07 +0100

    Projet 13/03 9:20

---
+++ Cactus project/Assets/Scripts/Background/QuickSand.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickSand : MonoBehaviour 
+{
+	public GameObject goat;
+	public GameObject goatInSand;
+	public Animator anim;
+	private bool isInSand = false;
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+
+		if(isInSand)
+		{
+			goat.SetActive (false);
+			goatInSand.SetActive (true);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "Goat")
+		{
+			isInSand = true;
+		}
+	}
+
+
+
+
+
+}
