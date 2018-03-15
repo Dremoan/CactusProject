1750a8157c94c893068463db830ebd043a7e7912
Unknown <Robin.seignolles@hotmail.fr>
2018-03-15 20:11:54 +0100

    Projet 15/03 20:11

---
+++ Cactus project/Assets/SlingShot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShot : MonoBehaviour {
+
+	public GoatInSand goatScript;
+	public GameObject player;
+	public LineRenderer lianeRend;
+
+
+	void Start () 
+	{
+		
+	}
+
+
+	void Update () 
+	{
+		if(goatScript.GetComponent<GoatInSand>().isCharging)
+		{
+			DrawLine ();
+		}
+		if(goatScript.GetComponent<GoatInSand>().isCharging == false)
+		{
+			lianeRend.enabled = false;
+		}
+	}
+
+	void DrawLine()
+	{
+		lianeRend.enabled = true;
+		lianeRend.SetPosition (0, transform.position);
+		lianeRend.SetPosition (1, player.transform.position);
+	}
+}
